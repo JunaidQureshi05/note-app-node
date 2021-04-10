37849d80e8daab8b30582b6f4a68d2c5a970262e
Junaid Qureshi <Junaid.562000@gmail.com>
2021-04-10 22:08:46 +0530

    dninveod

---
+++ app.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk');
+const getNotes = require('./notes.js');
+
+const command = process.argv[2];
+
+if (command === 'add') {
+  console.log('Adding Notes');
+} else {
+}
---
+++ utils.js
@@ -0,0 +1,6 @@
+// console.log('utils.js');
+// const name = 'Junaid';
+
+// const add = (a, b) => a + b;
+
+// module.exports = add;
